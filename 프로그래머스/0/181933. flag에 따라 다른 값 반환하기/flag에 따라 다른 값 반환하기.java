21c5d0cfa973fc120f19ba3a9d2cd8bd01bf9013
Denma5319 <firetrap5319@gmail.com>
2024-12-24 21:16:03 +0900

    [level 0] Title: flag에 따라 다른 값 반환하기, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int a, int b, boolean flag) {
+        
+        if(flag){
+            return a + b;    
+        }
+        else{
+            return a - b;
+        }
+        
+    }
+}
